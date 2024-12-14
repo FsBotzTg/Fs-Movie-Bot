de0ade233abad37fa60ba055b380136a7850cd4a
fahadshamhd <fahadshamuhammad321@gmail.com>
2024-12-14 07:29:24 +0000

    Update info.py

--- info.py
+++ info.py
@@ -155,7 +155,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "https://amused-ernesta-fs-botz-b0612d93.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
