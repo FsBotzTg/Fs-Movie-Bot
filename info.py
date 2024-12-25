2c6468f1779caf8e17ddd0ae92a706e83fa4534a
Fs Botz <fsbotztg@gmail.com>
2024-12-25 07:17:08 +0000

    Update info.py

--- info.py
+++ info.py
@@ -58,7 +58,7 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://firozshamuhammad12:1qa
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
-MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or False
+MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
 O_DB_URI = environ.get('O_DB_URI', "mongodb+srv://fsbotzusers:fsbotzusers@users.kq98g.mongodb.net/?retryWrites=true&w=majority&appName=Users")   # This Db Is For Other Data Store
